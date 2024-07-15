69b8d15535e44c622c597f952f5de47333aaca80
ola-ben <olaben09@gmail.com>
2024-07-15 11:01:19 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let age = 120;
+console.log(age);
+
+age = 24;
+
+console.log(age * 3);
+
+const b = "Ben";
+console.log(b);
+// b = "Role model";
+
+// PRIMITIVE DATA TYPE
+// String, number, boolean, null, undefined, symbol, BigInt
+
+const firstName = "Benjamin"; //String
+const birthYear = 200.9; // number
+const isOld = true;
+const bee = null;
+
+let you;
+
+console.log(you);
+
+// ARITHEmeic Operator
+let add = 4;
+// console.log(--add);
+console.log(add);
+
+// === or ==
+add += 4;
+// add = add + 4
+console.log(add);
